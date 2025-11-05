cea4d6a4925ee4c617e9fc0c803fb7f46959df69
unknown <ipb23.s.brikmane@vtdt.edu.lv>
2025-11-05 09:07:27 +0200

    Jāpielabo css

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,7 @@
 <header>
     <nav style="background-color: #6d1b1b; padding: 10px;">
         <a href="{{ url('/') }}" style="color: white; margin-right: 15px; text-decoration: none;">Sākums</a>
-        <a href="{{ url('/play') }}" style="color: white; margin-right: 15px; text-decoration: none;">Spēlēt</a>
+       <a href="{{ url('/play') }}" style="color: white; margin-right: 15px; text-decoration: none;">Memory-game</a>
+       <a href="{{ url('/typing-game') }}" style="color: white; margin-right: 15px; text-decoration: none;">Typing-game</a>
     </nav>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,27 @@
     
 </head>
 <body>
-     @include('components.navbar')
+    
 
     <h1>Sveiki, ko vēlies spēlēt!</h1>
-    <p><a href="{{ url('/play') }}">Sākt atmiņas kartīšu spēli</a></p>
-    <p><a href="{{ url('/typing-game') }}">ll</a></p>
+
+<div class="cards-wrapper">
+    <div class="card">
+        <img src="img/cards.png" alt="game" >
+        <div class="container">
+            <h4><b>Atmiņas spēle</b></h4>
+            <p><a href="{{ url('/play') }}">Sākt atmiņas kartīšu spēli</a></p>
+        </div>
+    </div>
+
+    <div class="card">
+        <img src="img/typing.jpg" alt="game" >
+        <div class="container">
+            <h4><b>Typing-game </b></h4>
+            <p><a href="{{ url('/typing-game') }}">Typing-game</a></p>
+        </div>
+    </div>
+</div>
+   
 </body>
-</html
+</html>
